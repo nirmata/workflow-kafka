d27f6824f0cdf19909718a81aa4f3f86fa1815cc
Abhishek Choudhary <shreemaan.abhishek@gmail.com>
2022-06-01 10:15:01 +0530

    add javadoc comment

--- src/main/java/com/nirmata/workflow/WorkflowManagerUtil.java
+++ src/main/java/com/nirmata/workflow/WorkflowManagerUtil.java
@@ -6,6 +6,22 @@ public class WorkflowManagerUtil {
     private WorkflowManagerUtil() {
     }
 
+    /**
+     * Create and return a Workflow variant based on the type of Workflow used by the client
+     * @see WorkflowManagerKafkaBuilder
+     * @see WorkflowManagerBuilder
+     *
+     * @param curator CuratorFramework
+     * @param kafkaAddress kafka broker address string
+     * @param namespace workflow namespace
+     * @param version workflow version
+     * @param useCuratorForWorkflow whether client uses curator for workflow. This value can be set using an
+     * environment variable which allows easy switching across the workflow types
+     *
+     * @return WorkflowManagerBaseBuilder (for chaining) returns a builder withCurator() if curator is being used
+     * by the client else returns a builder withKafka()
+     */
+
     public static WorkflowManagerBaseBuilder createBuilder(CuratorFramework curator, String kafkaAddress,
             String namespace, String version, boolean useCuratorForWorkflow) {
         if (useCuratorForWorkflow) {
